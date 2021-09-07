cf3af6f7b38ea9e37fdbc1b9d2d0374550111e06
AlirezaRezaei <cavalier.boy76@gmail.com>
2021-09-07 19:56:49 +0430

    add isPowerOfTwo problem

--- math-problem.js
+++ math-problem.js
@@ -9,7 +9,7 @@ function fibonacci(n) {
 //console.log(fibonacci(6)); ==>  [1,1,2,3,5,8]
 
 function isPrime(number) {
-    for (let i = 2; i <= Math.sqrt(number) ; i++) {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
         if (number % i == 0) {
             return false;
         }
@@ -19,3 +19,18 @@ function isPrime(number) {
 // isPrime(number) ==> O(Math.sqrt(number))
 // console.log(isPrime(9)) ==> false
 // console.log(isPrime(5)) ==> true
+
+function isPowerOfTow(number) {
+    if(number == 2){
+        return true;
+    }
+
+    if (number > 2) {
+        return isPowerOfTow(number / 2)
+    }
+
+    return false
+}
+// isPowerOfTow ==> O(logN)
+// console.log(isPowerOfTow(10)); ==> false
+// console.log(isPowerOfTow(8)); ==> true
